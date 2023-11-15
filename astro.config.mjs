d8c51fc1cf0a191c82182b68fac316b58759f7b5
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2023-11-15 13:49:41 -0500

    Remove base url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://ahalabs.dev',
-  base: '/soroban-tutorial-project',
-});
+export default defineConfig({});
